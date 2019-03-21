fa5c0f93af922a7369c6829911ec0efb86690b7e
nishanthrajan <47620995+nishanthrajan@users.noreply.github.com>
2019-03-21 10:25:49 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter the input a:");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	return 0;
+	getch();
+}
+ 
